4d68fa7c5fd38c281a8a207424f3a3939a0a2a8e
rjmccluskey <rjmccluskey@gmail.com>
2015-01-25 10:59:13 -0800

    config express-sessoin in app.js

--- source/app.js
+++ source/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -24,6 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'mongorulespostgresdrools',
+    resave: false,
+    saveUninitialized: true
+}));
 
 mongoose.connect(database.url)
 
