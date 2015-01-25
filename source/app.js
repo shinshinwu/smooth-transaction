872e2743120f27c136f9e0ba5b5c6623c666ec99
rjmccluskey <rjmccluskey@gmail.com>
2015-01-25 13:57:18 -0800

    convert views to ejs

--- source/app.js
+++ source/app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // view engine setup, replace with custom code
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
