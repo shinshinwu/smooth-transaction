bf201dd128ec8083f0e64d267374f3eecd6a3ef5
rjmccluskey <rjmccluskey@gmail.com>
2015-01-24 19:14:31 -0800

    init branch commit

--- stripe-node-test/app/models/user.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('User', {
-  stripe_user_id: String,
-  stripe_publishable_key: String,
-  refresh_token: String,
-  access_token: String
-});
