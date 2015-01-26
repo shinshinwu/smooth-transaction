3dbef36fa117f385053494c7aff8993f8f1a40ce
rjmccluskey <rjmccluskey@gmail.com>
2015-01-26 11:36:51 -0800

    add email, EIN, and phone validations

--- source/app/models/user.js
+++ source/app/models/user.js
@@ -1,36 +1,69 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs')
 var uniqueValidator = require('mongoose-unique-validator');
+var validate = require('mongoose-validator');
 
+
+// validators:
+
+
+var emailValidator = [
+  validate({
+    validator: 'isEmail',
+    message: 'Please enter a valid email'
+  })
+];
+
+var einValidator = [
+  validate({
+    validator: 'matches',
+    arguments: /\d{2}[-]?\d{7}/,
+    message: 'Please enter a valid 9 digit EIN'
+  })
+]
+
+var phoneValidator = [
+  validate({
+    validator: 'matches',
+    arguments: /[(]?\d{3}([-]|[)])?\d{3}[-]?\d{4}\d*/,
+    message: 'Please enter at least a ten digit phone number'
+  })
+]
+
+
+
+
+
+// Schema:
 var userSchema = new mongoose.Schema({
 
-  email                 : {type: String, required: true, unique: true},
+  email                 : {type: String, required: true, unique: true, validate: emailValidator},
 
   password              : {type: String, required: true},
 
-  orgName               : {type: String, required: true},
+  orgName               : {type: String /*required: true*/},
 
-  EIN                   : {type: String, required: true},
+  EIN                   : {type: String, validate: einValidator /*required: true*/},
 
   address               : {
-                            street: {type: String, required: true},
-                            zip: {type: String, required: true}
+                            street: {type: String /*required: true*/},
+                            zip: {type: String /*required: true*/}
                           },
 
-  website               : {type: String, required: true},
+  website               : {type: String /*required: true*/},
 
   representative        : {
-                            firstName: {type: String, required: true},
-                            lastName: {type: String, required: true},
+                            firstName: {type: String /*required: true*/},
+                            lastName: {type: String /*required: true*/},
                             DOB: {
-                              day: {type: String, required: true},
-                              month: {type: String, required: true},
-                              year: {type: String, required: true}
+                              day: {type: String /*required: true*/},
+                              month: {type: String /*required: true*/},
+                              year: {type: String /*required: true*/}
                             },
-                            SSN: {type: String, required: true}
+                            SSN: {type: String /*required: true*/}
                           },
 
-  phone                 : {type: String, required: true},
+  phone                 : {type: String, validate: phoneValidator /*required: true*/},
 
   stripe_user_id        : {type: String, default: ''},
 
@@ -50,6 +83,10 @@ var userSchema = new mongoose.Schema({
 
 userSchema.plugin(uniqueValidator);
 
+
+
+
+// Before save, convert password into bcrypt hash
 userSchema.pre('save', function(next) {
   var user = this;
 
@@ -67,6 +104,7 @@ userSchema.pre('save', function(next) {
   });
 });
 
+// define method for checking a password against the bcrypt hash
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
     if (err) {
