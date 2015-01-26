9777bfb3f5ce4024a216c7c34f858fefb915f620
rjmccluskey <rjmccluskey@gmail.com>
2015-01-25 17:50:25 -0800

    sessions working for login and sign up

--- source/routes/index.js
+++ source/routes/index.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res) {
     },
       // if successful
       function(user) {
-        res.render('dashboard', {user: user})
+        res.redirect('/dashboard')
     });
   }
   else {
@@ -64,22 +64,38 @@ router.get('/logout', function(req, res) {
   res.redirect('/')
 });
 
-router.get('/users', function(req, res) {
+router.get('/dashboard', function(req, res) {
   var email = req.param('email');
   var password = req.param('password');
+  var userId = req.session.user_id
 
-  User.findOne({ 'email': email }, function(err, user) {
-    if (err) {
-      res.redirect('/?err=' + err)
-    }
-    else if (user.password === password) {
-      res.render('dashboard', {user: user})
-    }
-    else {
-      err = 'Invalid email and password!'
-      res.redirect('/?err=' + err)
-    }
-  });
+  if (userId) {
+    // if logged in, render dashboard page
+    getUser(userId,
+      // if error....
+      function(err) {
+        res.redirect('/?err=' + err);
+    },
+      // if successful
+      function(user) {
+        res.render('dashboard', {user: user})
+    });
+  }
+  else {
+    User.findOne({ 'email': email }, function(err, user) {
+      if (err) {
+        res.redirect('/?err=' + err)
+      }
+      else if (user.password === password) {
+        req.session.user_id = user._id
+        res.render('dashboard', {user: user})
+      }
+      else {
+        err = 'Invalid email and password!'
+        res.redirect('/?err=' + err)
+      }
+    });
+  }
 });
 
 router.post('/users', function(req, res) {
